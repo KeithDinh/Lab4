08c6fb5ad661a9d66bfc12618305dc70450d084b
Keith Dinh <52475783+KeithDinh@users.noreply.github.com>
2021-06-13 16:47:30 -0500

    first commit

---
+++ NewLanguageFeatures/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewLanguageFeatures
+{
+    public class Point
+    {
+        List<Point> Square = new List<Point>
+        {
+            new Point { X=0, Y=5 },
+            new Point { X=5, Y=5 },
+            new Point { X=5, Y=0 },
+            new Point { X=0, Y=0 }
+        };
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
